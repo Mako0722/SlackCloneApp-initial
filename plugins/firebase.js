c67c1b95d470f54fe5b0b538c8ce6e19dc4b9d24
Mako0530 <zhongtiancheng79@gmail.com>
2021-04-08 17:20:58 +0900

    スペース修正

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,17 +2,17 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-if (!firebase.apps.length) {
- const config = {
-    apiKey: "AIzaSyCTm_dafHc8w3zNCNaehey9JgVxp40iBMI",
-    authDomain: "slackcloneapp-initial.firebaseapp.com",
-    projectId: "slackcloneapp-initial",
-    storageBucket: "slackcloneapp-initial.appspot.com",
-    messagingSenderId: "228367313662",
-    appId: "1:228367313662:web:b3a136aeaaf0fc6a4913af",
-    measurementId: "G-LX9X7EH09F"
- }
- firebase.initializeApp(config)
+if  (!firebase.apps.length) {
+   const config = {
+      apiKey: "AIzaSyCTm_dafHc8w3zNCNaehey9JgVxp40iBMI",
+      authDomain: "slackcloneapp-initial.firebaseapp.com",
+      projectId: "slackcloneapp-initial",
+      storageBucket: "slackcloneapp-initial.appspot.com",
+      messagingSenderId: "228367313662",
+      appId: "1:228367313662:web:b3a136aeaaf0fc6a4913af",
+      measurementId: "G-LX9X7EH09F"
+   }
+   firebase.initializeApp(config)
 }
 
 const db = firebase.firestore()

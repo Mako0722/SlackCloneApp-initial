61691f0fc4239d7de5953d107051649341e0a81a
Mako0530 <zhongtiancheng79@gmail.com>
2021-04-08 14:14:16 +0900

    v-bind利用 v-on利用 入力時にログインしろよと表示

--- components/Message.vue
+++ components/Message.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="chat-container">
     <div class="thumbnail-container">
-      <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
+      <img v-bind:src="message.user.thumbnail" />
     </div>
     <div class="message-container">
-      <div class="user-name">cohki0305</div>
-      <div class="message">今日も良い天気ですね</div>
+      <div class="user-name">{{ message.user.name }}</div>
+      <div class="message">{{ message.text }}</div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: {
+        text: '今日もいい天気ですね',
+        user: {
+          thumbnail: 'https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg',
+          name: 'note'
+        }
+      }
+    }
+  },
+  computed: {
+    displayName() {
+      return `@s{this.message.user.name}`
+    }
+  }
+}
+</script>
+
 
 <style scoped>
 .chat-container {
